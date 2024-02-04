27c7ea9e14a038019cda7224ad03f20523d0176f
fabio.henrique <fabio.henrique@hotmart.com>
2024-02-04 11:22:36 -0300

    init: implementaion send msg to aws sqs.

---
+++ main.py
@@ -0,0 +1,38 @@
+import json
+import boto3
+import datetime
+
+sqs = boto3.resource("sqs")
+
+
+def serialize_datetime(obj):
+    if isinstance(obj, datetime.datetime):
+        return obj.isoformat()
+    raise TypeError("Type not serializable")
+
+
+if __name__ == "__main__":
+    # data = open('resources/jsons/person-individual.json')
+    # data = open('resources/jsons/person-legal.json')
+    # data = open('resources/jsons/receive-account.json')
+    data = open('resources/jsons/test.json')
+    data = json.load(data)
+
+    # queue = sqs.get_queue_by_name(QueueName="sqs-data-account-processor-test")
+    queue = sqs.get_queue_by_name(QueueName="lambda_data_account_normalizer")
+
+    # Late
+    entries = [
+        {
+            "Id": str(ind),
+            "MessageBody": json.dumps(data.get('payload')),
+            "MessageAttributes": data.get('attributes'),
+        }
+        for ind in range(10)
+        # for ind, msg in enumerate(["1", "2", "3", "4"])
+
+    ]
+
+    # queue.send_messages(Entries=entries)
+    queue.send_message(MessageBody=json.dumps(data.get('payload'), default=serialize_datetime),
+                       MessageAttributes=data.get('attributes'))
